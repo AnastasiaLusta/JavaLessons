09c51713af953ce047a63d3259b671ad0db00cf5
AnastasiaLusta <itssialusta@gmail.com>
2022-09-20 19:53:35 +0300

    init

---
+++ src/step/learning/Main.java
@@ -0,0 +1,17 @@
+package step.learning;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ConsoleColors.GREEN);
+//        System.out.println(ConsoleColors.GREEN + "Hello world!");
+//        String s1 = new String( "Hello");
+//        String s2 = new String( "Hello");
+//        if(s1 == s2)
+//            System.out.println("==");
+//        else
+//            System.out.println("!==");
+//        new Complex().Run();
+        new Translater().Run();
+    }
+}
